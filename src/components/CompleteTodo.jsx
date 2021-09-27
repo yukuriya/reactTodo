508f8d1bbd031ae14fc06f8dc4d3a8e731c308cd
yukuriya <64369321+yukuriya@users.noreply.github.com>
2021-09-27 22:39:41 +0900

    Initial commit

---
+++ src/components/CompleteTodo.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const CompleteTodo = (props) => {
+  const { completeTodos, onClickBack } = props;
+  return (
+    <div className="complete-area">
+      <p className="title">完了済みのTODO</p>
+      <ul>
+        {completeTodos.map((todo, index) => {
+          return (
+            <div className="list-row">
+              <li>{todo}</li>
+              <button onClick={() => onClickBack(index)}>戻す</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
